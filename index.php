019238e1c6cabe6ef84a30194fdb21d09ab77ac4
gicunho <gicunho@hotmail.it>
2021-06-23 09:21:08 +0200

    Corretto esercizio con aggiunta $_GET

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
-$paragrafo = 'Questo è un paragrafo di prova dove proverò a usare delle parole che saranno sostituite in quanto parolacce, cazzo!';
+$paragrafo = 'Questo è un paragrafo di prova dove proverò a usare delle parole che saranno sostituite in quanto parolacce, caspita!';
 var_dump($paragrafo);
+//$richiesta_censura_msg = "Inserisci la parola da censurare";
 ?>
 
 <!DOCTYPE html>
@@ -14,10 +15,14 @@ var_dump($paragrafo);
 <body>
     <h1>PHP Bad Words</h1>
     <p>
-        <?php echo $paragrafo; ?>
-        <?php echo strlen($paragrafo); ?>
-        <?php echo str_replace('cazzo', '***', $paragrafo); ?>
-
+        <?php echo $paragrafo; ?> <br>
+        La lunghezza del paragrafo originale è di <?php echo strlen($paragrafo); ?> caratteri. <br>
+        <?php echo str_replace($_GET['censura'], '***', $paragrafo); ?>
     </p>
+    <form action="" method="get">
+    <input type="text" name="censura" placeholder="inserisci la parola da censurare">
+    <input type="submit" value="Submit">
+    </form>
+    
 </body>
 </html>
